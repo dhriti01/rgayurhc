f74383cb25475b2ec1dc0e90022059335d4ee84e
Dhriti <gupta.dhriti01@gmail.com>
2023-06-21 00:32:06 +0530

    add styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,6 +7,7 @@ import Footer from "@/components/Footer";
 
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../app/customTheme";
+import "../app/globals.css";
 
 export default function Layout(props) {
   const [isLoading, setIsLoading] = useState(true);
